d442264c2dede5dfd8d4859eb35984a7155623e7
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-08 15:58:05 +0530

    commit

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,8 +38,8 @@
 |
 */
 
-$route['default_controller'] = "welcome";
-//$route['default_controller'] = "welcome";
+// $route['default_controller'] = "welcome";
+$route['default_controller'] = "you_view";
 $route['404_override'] = '';
 
 
--- application/controllers/hello.php
+++ application/controllers/hello.php
@@ -1,8 +1,14 @@
-<?php
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Hello extends CI_Controller {
 
-	
+	public function index()
+	{
+		 echo "Hi, all";
+		//$this->load->view('you_view');
+	}
+
+
 	public function you()
 	{
 		// echo "this is you";
