fdd44dcc40bdf1c01f7b784e09dfe62075f2c602
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-22 15:48:20 +0530

    test 2

--- application/controllers/azurepage.php
+++ application/controllers/azurepage.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Hello extends CI_Controller{
+class Azurepage extends CI_Controller{
 
 	public function index(){
 		// echo "You are in index";
