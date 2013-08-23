e990513d9e39d804e95eadd2866e33d83379dea6
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-23 20:15:34 +0530

    4.26

--- script/newsletter-form.js
+++ script/newsletter-form.js
@@ -6,7 +6,7 @@
 	{
 		console.info("hello all...");
         blockForm('newsletter-form','block');
-	    $.post('<?= base_url('Newsletter-form/newsletterform'); ?>'),$('#newsletter-form').serialize(),submitNewsletterFormResponse,'json');
+	    $.post('<?= base_url('newsletter-form.php'); ?>'),$('#newsletter-form').serialize(),submitNewsletterFormResponse,'json');
 	}
 	
 	/*****************************************************************/
