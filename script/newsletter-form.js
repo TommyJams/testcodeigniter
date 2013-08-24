5c16b954c73f55e43d81112c86e5943c94586a5f
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-24 16:09:35 +0530

    newsletter change 5.12

--- script/newsletter-form.js
+++ script/newsletter-form.js
@@ -6,7 +6,7 @@
 	{
 		console.info("hello all...");
         blockForm('newsletter-form','block');
-	    $.post('/newsletter.php',$('#newsletter-form').serialize(),submitNewsletterFormResponse,'json');
+	    $.post('/../application/controllers/newsletter.php',$('#newsletter-form').serialize(),submitNewsletterFormResponse,'json');
     }
 	
 	/*****************************************************************/
