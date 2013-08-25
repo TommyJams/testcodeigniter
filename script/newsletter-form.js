4bad67a833056a5c84e54ac2dd65c10febf7c3f3
TommyJams <nikhilk218@gmail.com>
2013-08-25 17:20:17 +0530

    newsletter change 5.50

--- script/newsletter-form.js
+++ script/newsletter-form.js
@@ -6,7 +6,7 @@
 	{
 		console.info("submitting");
         blockForm('newsletter-form','block');
-	    $.post('<?php echo base_url();?>/newsletter/validateData',$('#newsletter-form').serialize(),submitNewsletterFormResponse,'json');
+	    $.post('/newsletter/validateData',$('#newsletter-form').serialize(),submitNewsletterFormResponse,'json');
     }
 	
 	/*****************************************************************/
