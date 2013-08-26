da16aed160d94475f6fa78a53d3fb8f9a2bde421
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-26 12:01:13 +0530

    newsletter main change 1.1

--- application/controllers/newsletter.php
+++ application/controllers/newsletter.php
@@ -1,10 +1,10 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-	class Newsletter extends CI_Controller{
+	class Newsletter_2 extends CI_Controller{
 	
 		public function validateData(){
 	
-			this->load->helper('functions');
+			$this->load->helper('functions');
 			$response=array('error'=>0,'info'=>null);
 
 			$values=array
@@ -12,7 +12,8 @@
 				'newsletter-form-mail'		=> $_POST['newsletter-form-mail']
 			);
 	
-			if(isGPC()) $values=array_map('stripslashes',$values);
+			if(isGPC()) 
+				$values=array_map('stripslashes',$values);
 	
 			/**************************************************************************/
 	
@@ -63,7 +64,7 @@
     		{
         		/************* This code is for MailChimp Integration ****************/
         		//require_once('MCAPI.class.php');
-        		this->load->library('MCAPI.class');
+        		$this->load->library('MCAPI.class');
 
         		// API Key: http://admin.mailchimp.com/account/api/
         		$api = new MCAPI('4b1d3dfd9a40c3a47861fa481d644505-us5');
