e5cfb706f36a4b107d6b4651dda2bab1a5d9b6fb
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-26 21:00:29 +0530

    twitter change 3.2

--- application/controllers/tweet_test.php
+++ application/controllers/tweet_test.php
@@ -2,7 +2,7 @@
 
 	class Tweet_test extends CI_Controller {
 		
-		function __construct()
+	/*	function __construct()
 		{
 			parent::__construct();
 			
@@ -40,14 +40,35 @@
 				// if you already have your own auth system.
 			}
 		}
-		
-	/*	function index()
-		{
-			echo 'hi there';
-		}
 		*/
-		function auth()
+	
+		public function auth()
 		{
+			// It really is best to auto-load this library!
+			$this->load->library('tweet');
+			
+			// Enabling debug will show you any errors in the calls you're making, e.g:
+			$this->tweet->enable_debug(TRUE);
+			
+			// If you already have a token saved for your user
+			// (In a db for example) - See line #37
+			// 
+			// You can set these tokens before calling logged_in to try using the existing tokens.
+			// $tokens = array('oauth_token' => 'foo', 'oauth_token_secret' => 'bar');
+			// $this->tweet->set_tokens($tokens);
+			
+			
+			if ( !$this->tweet->logged_in() )
+			{
+				// This is where the url will go to after auth.
+				// ( Callback url )
+				
+				$this->tweet->set_callback(site_url('tweet_test/auth'));
+				
+				// Send the user off for login!
+				$this->tweet->login();
+			}
+
 			$tokens = $this->tweet->get_tokens();
 			
 			// $user = $this->tweet->call('get', 'account/verify_credentiaaaaaaaaals');
