211724cbc322c783d4192daebcaef6a3507e698e
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-27 19:53:54 +0530

    library change 1

--- application/controllers/newsletter.php
+++ application/controllers/newsletter.php
@@ -66,7 +66,7 @@
         		$config = array(
 	    				'apikey' => '4b1d3dfd9a40c3a47861fa481d644505-us5' );
 
-				$this->load->library('MCAPI', $config, 'mail_chimp');
+				$this->load->library('mailchimp/MCAPI', $config, 'mail_chimp');
 
         		// API Key: http://admin.mailchimp.com/account/api/
         		// $api = new MCAPI('4b1d3dfd9a40c3a47861fa481d644505-us5');
