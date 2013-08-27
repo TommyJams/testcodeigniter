449564c2db2163f4b7e17a00a5bf323b18ab189c
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-27 21:04:44 +0530

    twitter lib change 1.4

--- application/controllers/twitterproxy.php
+++ application/controllers/twitterproxy.php
@@ -6,24 +6,24 @@ class Twitterproxy extends CI_Controller{
 
 		session_start();
 
-		$config = array( 
+		/*	$config = array( 
 					'twitteruser' => 'tommyjams',
                   	'notweets'  => 10,
                   	'consumerkey' => 'Fh22VgGqVUgoFfhn4uADdw',
                   	'consumersecret' => 'slSgXgHTTgi6z2cFl8dfWIwILM3njiWfQHaF8cE0xA"',
                   	'accesstoken' => '630529499-sWjOJ9pXm9qufutOCUdw8iq0exSFj2RSoClsauSq',
                   	'accesstokensecret' => 'iLsAk65xqzn8nXWmVul9gp33SvVz5AxN2UK1Lb7ew',
-                  );
+                  ); */
 
-		$this->load->library('twitteroauth/TwitterOAuth', $config);
+		$this->load->library('twitteroauth/TwitterOAuth');
 		// require_once("twitteroauth/twitteroauth.php"); //Path to twitteroauth library
- 		/*
+ 		
 		$twitteruser = "tommyjams";
 		$notweets = 10;
 		$consumerkey = "Fh22VgGqVUgoFfhn4uADdw";
 		$consumersecret = "slSgXgHTTgi6z2cFl8dfWIwILM3njiWfQHaF8cE0xA";
 		$accesstoken = "630529499-sWjOJ9pXm9qufutOCUdw8iq0exSFj2RSoClsauSq";
-		$accesstokensecret = "iLsAk65xqzn8nXWmVul9gp33SvVz5AxN2UK1Lb7ew"; */
+		$accesstokensecret = "iLsAk65xqzn8nXWmVul9gp33SvVz5AxN2UK1Lb7ew"; 
  
 		function getConnectionWithAccessToken($cons_key, $cons_secret, $oauth_token, $oauth_token_secret) {
   			$connection = new TwitterOAuth($cons_key, $cons_secret, $oauth_token, $oauth_token_secret);
@@ -34,7 +34,10 @@ class Twitterproxy extends CI_Controller{
  
 		$tweets = $connection->get("https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=".$twitteruser."&count=".$notweets);
  
-		echo json_encode($tweets);
+ 		$output = json_encode($anobject);
+    	$this->output->set_content_type('application/json');
+    	$this->output->set_output($output);
+		// echo json_encode($tweets);
 
 	}
 }
