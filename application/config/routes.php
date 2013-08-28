c7d93a0f98a39127d8303a983a316b993125446e
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-28 20:21:56 +0530

    beta change 1.7

--- application/config/routes.php
+++ application/config/routes.php
@@ -40,7 +40,7 @@
 
 // $route['default_controller'] = "welcome";
 $route['default_controller'] = "azurepage";
-$route['betapage'] = "betapage/betalandingpage";
+$route['betapage/(:any)'] = "betapage/betalandingpage";
 $route['404_override'] = '';
 
 
