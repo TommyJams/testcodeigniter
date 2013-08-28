1c41275632005cb2975e396e1c9550c56d05b4b8
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-28 13:41:10 +0530

    contact lib change 1.5

--- application/views/contactdefault_view.php
+++ application/views/contactdefault_view.php
@@ -6,9 +6,9 @@
 
 	<body>
 
-		<div><b>Name</b>: <?php echo $this->data['contact-form-name']; ?></div>
-		<div><b>E-mail</b>: <?php echo $this->data['contact-form-mail']; ?></div>
-		<div><b>Message</b>: <?php echo nl2br($this->data['contact-form-message']); ?></div>
+		<div><b>Name</b>: <?php echo $key['contact-form-name']; ?></div>
+		<div><b>E-mail</b>: <?php echo $key['contact-form-mail']; ?></div>
+		<div><b>Message</b>: <?php echo nl2br($key['contact-form-message']); ?></div>
 
 	</body>
 
