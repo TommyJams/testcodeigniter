12d10230bb7a4b0341b13f4872c8e7895ea14a5a
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-28 13:46:59 +0530

    contact lib change 1.6

--- application/views/contactdefault_view.php
+++ application/views/contactdefault_view.php
@@ -6,9 +6,9 @@
 
 	<body>
 
-		<div><b>Name</b>: <?php echo $key['contact-form-name']; ?></div>
-		<div><b>E-mail</b>: <?php echo $key['contact-form-mail']; ?></div>
-		<div><b>Message</b>: <?php echo nl2br($key['contact-form-message']); ?></div>
+		<div><b>Name</b>: <?php echo $contactformname; ?></div>
+		<div><b>E-mail</b>: <?php echo $contactformmail; ?></div>
+		<div><b>Message</b>: <?php echo nl2br($contactformmessage); ?></div>
 
 	</body>
 
