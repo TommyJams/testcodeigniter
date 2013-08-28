7441d5644a2bf465a835f49ff521554e1727cc38
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-28 14:25:35 +0530

    contact lib change 1.10

--- application/views/contactdefault_view.php
+++ application/views/contactdefault_view.php
@@ -6,10 +6,14 @@
 
 	<body>
 
-		<div><b>Name</b>: <?php echo $contact-form-name; ?></div>
-		<div><b>E-mail</b>: <?php echo $values['contact-form-name']; ?></div>
-		<div><b>Message</b>: <?php echo nl2br($values['contact-form-name']); ?></div>
+		<ul>
+			<?php foreach ($values as $item):?>
 
+			<div><?php echo $item;?></div>
+
+			<?php endforeach;?>
+		</ul>
+		
 	</body>
 
 </html>
