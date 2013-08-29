26c83d3731d97e9861833eac273ccffd2b923ff7
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-29 20:08:09 +0530

    fbconnect 1.11

--- script/main.js
+++ script/main.js
@@ -246,7 +246,7 @@
 		{
 			// init the FB JS SDK
 			FB.init({
-			  appId      : '204029036428158', // App ID from the App Dashboard
+			  appId      : '345757728821408', // App ID from the App Dashboard
 			  channelUrl : '//testcodeigniter.azurewebsites.net/channel.html', // Channel File for x-domain communication
 			  status     : true, // check the login status upon init?
 			  cookie     : true, // set sessions cookies to allow your server to access the session?
