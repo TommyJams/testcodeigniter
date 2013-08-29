1ed6eab189df36bbb400951888071d242e714511
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-29 20:01:38 +0530

    fbconnect change 1.10

--- application/views/fbConnect1_view.php
+++ application/views/fbConnect1_view.php
@@ -53,7 +53,7 @@
 
 					<iframe src="https://www.facebook.com/plugins/registration?
 								client_id=<?php echo $appId;?>&
-				 				redirect_uri=http://testcodigniter.azurewebsites.net/fbconnect.php&
+				 				redirect_uri=http://testcodigniter.azurewebsites.net/fbconnect&
 				 				fb_only=true&
 				 				fb_register=true&
 				 				fields=<?php echo $fb_fields;?>"
