9e763749a3e46f54cb5b91aa148cf860c8de9398
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-30 12:33:58 +0530

    fbconnect change 2.6

--- application/controllers/fbconnect.php
+++ application/controllers/fbconnect.php
@@ -98,7 +98,7 @@ class Fbconnect extends CI_Controller{
   			$data1['fb_fields']="[{'name':'name'},{'name':'email'},{'name':'location'},{'name':'birthday'},{'name':'usertype','description':'User Type','type':'select','options':{'artist':'Artist','venue':'Venue','promoter':'Promoter'},'default':'artist'},{'name':'org','description':'Band/Organisation Name','type':'text'},{'name':'phone','description':'Phone Number','type':'text'},]";
   			$data1['appId']= FACEBOOK_APP_ID;
 
-        $this->input->get('registered')
+        $this->input->get('registered');
 
   			if ($_GET['registered']=='no')
 			 	   $this->load->view('fbConnect1_view', $data1);
