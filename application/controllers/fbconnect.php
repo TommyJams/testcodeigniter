025a66ae3b0f3303f5f34c2b91c16bda981b6c58
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-30 15:57:01 +0530

    fbconnect change 2.12

--- application/controllers/fbconnect.php
+++ application/controllers/fbconnect.php
@@ -97,136 +97,14 @@ class Fbconnect extends CI_Controller{
   			
   			$data1['fb_fields']="[{'name':'name'},{'name':'email'},{'name':'location'},{'name':'birthday'},{'name':'usertype','description':'User Type','type':'select','options':{'artist':'Artist','venue':'Venue','promoter':'Promoter'},'default':'artist'},{'name':'org','description':'Band/Organisation Name','type':'text'},{'name':'phone','description':'Phone Number','type':'text'},]";
   			$data1['appId']= FACEBOOK_APP_ID;
+        $data1['iframe']=$this->load->view('registration1_view', NULL, TRUE);
 
         $this->input->get('registered');
 
   			if ($_GET['registered']=='no')
 			 	   $this->load->view('fbConnect1_view', $data1);
 
-        elseif ($_GET['registered']=='fbregistered') //User registered just now
-        {
-            //enter into database
-            if ($_REQUEST) 
-            {
-              //echo '<p>signed_request contents:</p>';
-              $response = parse_signed_request($_REQUEST['signed_request'], FACEBOOK_SECRET);
-              //echo '<pre>';
-              //print_r ($response);
-              //echo '</pre>';
-              
-              //$verification = check_registration($response,$fb_fields);
-              if($response)
-              {
-                $fbid=$response["user_id"]; 
-                //  include("connect.php");
-                $this->load->helper('connect');          
-                $query_check1 = "SELECT * FROM `$database`.`members` WHERE fb_id = '$fbid'";
-                $result_check1 = mysql_query($query_check1);  
-
-                //if user is logged in on facebook but has never registered on tommyjams
-                if(mysql_num_rows($result_check1) != 0)
-                {
-                  print ("
-                    <br><br>
-                    You are already registered with us. Try logging in again. In case of any issues, please contact us.
-                    <br><br><br>
-                    <center><a href='index.php' style='width:120px; height:20px; background:#ffcc00;'>Continue</a></center>
-                  ");
-                }
-                else
-                {
-                  $email=$response["registration"]["email"];
-                  $password=rand(111111,9999999);
-                  //$password=$response["registration"]["password"];
-                  $password=md5($password);
-                  $username=mysql_real_escape_string($response["registration"]["name"]);
-                  $city_country=$response["registration"]["location"]["name"];
-                  $split=explode(",", $city_country); //Eg. Split "Bangalore, India" into "Bangalore" and "India"
-                  if ($split[2]) //Eg. "Bankok, Krung Thep, Thailand"
-                  {
-                    $city=addslashes($split[0]);
-                    $state=trim($split[1]);
-                    $state=addslashes($state);
-                    $country=trim($split[2]);
-                    $country=addslashes($country);
-                  }
-                  else //Eg. "Bangalore, India"
-                  {
-                    $city=addslashes($split[0]);
-                    $state="";
-                    $country=trim($split[1]);
-                    $country=addslashes($country);
-                  }
-                  $you=$fbid;
-                  $birth=$response["registration"]["birthday"];
-                  $fb=addslashes('http://www.facebook.com/').$fbid;
-                  $gender=$response["registration"]["gender"];
-                  $phone=$response["registration"]["phone"];
-                  $organization=mysql_real_escape_string($response["registration"]["org"]);
-                  $actual_type=$response["registration"]["usertype"];
-                  if($actual_type=='promoter'){$what="Promoter";}
-                  elseif($actual_type=='venue'){$what="Promoter";}
-                  elseif($actual_type=='artist'){$what="Artist";}
-                  else{$what="Artist";}
-
-                  //print_r($user_profile);
-                  $fb_username=mysql_real_escape_string($user_profile["username"]);
-                  $about=$user_profile["bio"];
-                  $about=addslashes($about);
-                  /*
-                  $emp=$user_profile["work"]["0"]["employer"]["name"];
-                  $emp=addslashes($emp);
-                  $emp_position=$user_profile["work"]["0"]["position"]["name"];
-                  $emp_position=addslashes($emp_position);
-                  $student="Student";
-                  $student=$user_profile["education"]["0"]["school"]["name"];
-                  $student=addslashes($student);
-                  if($emp!=""){$job="Work as"; $organization=$emp;}else{$job="Studying"; $organization=$student;}
-                  */
-                  
-                  // include("connect.php");
-                  $this->load->helper('connect');
-                  /*
-                  $SQLs = "SELECT id FROM `$database`.`members`";
-                  $results = mysql_query($SQLs);
-                  while ($a = mysql_fetch_assoc($results))
-                  {
-                    $id=$a["id"];
-                  }
-                  $id=$id+1;
-                  $ida=$id*15993;
-                  $link="$ida";
-                  */
-                  $ip=$_SERVER['REMOTE_ADDR'];
-
-                  $query = "INSERT INTO `$database`.`members` (`id`, `type`, `actual_type`, `dob`, `name`, `username`, `fb_username`,`password`, `email`, `mobile`, `fb_id`, `city`, `state`,`country`, `about`, `gender`, `fb`, `status`, `job`, `user`, `ip`, `time`)
-                                     VALUES (NULL, '$what', '$actual_type', '$birth', '$organization', '$username', '$fb_username', '$password', '$email', '$phone', '$fbid', '$city', '$state', '$country', '$about', '$gender', '$fb', '1', '$job', '$fbid', '$ip', CURRENT_TIMESTAMP)";
-
-                  $ress = mysql_query($query);
-                  if (!$ress)
-                  {
-                    echo 'Database query failed:'. mysql_error();
-                  }
-                  else
-                  {
-                    print ("
-                    <br><br>
-                    You are successfully registered
-                    <br><br><br>
-                    <table border=0>
-                      <tr><td width=150>Band/Organisation</td><td>$organization</td></tr>
-                      <tr><td width=150>User Name</td><td>$username</td></tr>
-                      <tr><td>Email</td><td>$email</td></tr>
-                      <tr><td>City</td><td>$city</td></tr>
-                    </table>
-                    <br><br><br>
-                    <center><a href='fbconnect?registered=yes' style='width:120px; height:20px; background:#ffcc00;'>Continue</a></center>
-                    "); 
-                  }
-                }    
-              }    
-            } 
-          }     	
+            	
   	}
 } 	
 ?>
--- application/views/fbConnect1_view.php
+++ application/views/fbConnect1_view.php
@@ -51,19 +51,8 @@
 
 				<div id="textContainer">
 
-					<iframe src="https://www.facebook.com/plugins/registration?
-								client_id=<?php echo $appId;?>&
-				 				redirect_uri=http://testcodeigniter.azurewebsites.net/fbconnect?c=fbconnect&m=connectFb&registered=fbregistered&
-				 				fb_only=true&
-				 				fb_register=true&
-				 				fields=<?php echo $fb_fields;?>"
-							scrolling="auto"
-							frameborder="no"
-							style="border:none"
-							allowTransparency="true"
-							width="100%"
-							height="330">
-					</iframe>
+					<?php echo $iframe;?>
+					
 				</div>
 			</div>
 		</div>
