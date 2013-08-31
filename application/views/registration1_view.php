82dc9c636fb144f089f621efadd5bf14bc576865
TommyJams <tommyjams.bizspark@outlook.com>
2013-08-31 14:44:34 +0530

    fbconnect change 2.25

--- application/views/registration1_view.php
+++ application/views/registration1_view.php
@@ -1,6 +1,6 @@
 <iframe src="https://www.facebook.com/plugins/registration?
 		client_id=<?php echo $appId;?>&
-		redirect_uri='http://testcodeigniter.azurewebsites.net/fbconnect?c=fbconnect&m=connectFb&registered=fbregistered'&
+		redirect_uri="http://testcodeigniter.azurewebsites.net/fbconnect?c=fbconnect&m=connectFb&registered=fbregistered"&
 		fb_only=true&fb_register=true&fields=<?php echo $fb_fields;?>"
 	scrolling="auto"
 	frameborder="no"
@@ -8,5 +8,6 @@
 	allowTransparency="true"
 	width="100%"
 	height="330">
+
 
 </iframe>
