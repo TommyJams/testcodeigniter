0468dd752da9d71b07afd087101fa08873ab0d3e
TommyJams <nikhilk218@gmail.com>
2013-09-08 03:09:09 +0530

    dib action 1.14

--- application/controllers/artist.php
+++ application/controllers/artist.php
@@ -489,7 +489,7 @@ class Artist extends CI_Controller{
 
 		if(!isset($gigLink))
 		{
-			$response='error';
+			$response['status']='error';
 			createResponse($response);
 		}
 
@@ -535,7 +535,7 @@ class Artist extends CI_Controller{
 			$result_set4 = mysql_query($q4);	
 			if (mysql_num_rows($result_set4) == 1) 
 			{
-				$response='error';
+				$response['status']='error';
 				createResponse($response);
 			}
 
@@ -544,7 +544,7 @@ class Artist extends CI_Controller{
 			if (!$result_set2)
 			{
 				die("Database query failed: " . mysql_error());
-				$response='error';
+				$response['status']='error';
 				createResponse($response);
 			}
 
@@ -618,12 +618,11 @@ class Artist extends CI_Controller{
 			$to = "alerts@tommyjams.com";
 			send_email($to, $sender, $subject, $mess);
 
-			$response="ok";
-			error_log("dIB RESPONSE: ".$response);
+			$response['status']="ok";
 			createResponse($response);
 		}
 
-		$response="ok";
+		$response['status']="ok";
 		createResponse($response);
 	}
 
