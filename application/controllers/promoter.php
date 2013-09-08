94a32cd671fce6ec0e6817f9714dbb78f2304768
TommyJams <tommyjams.bizspark@outlook.com>
2013-09-08 15:50:14 +0530

    dib reaction 2.5

--- application/controllers/promoter.php
+++ application/controllers/promoter.php
@@ -230,33 +230,36 @@ public function mygigs(){
     	$desc=$a["desc"];$budget_min=$a["budget_min"];$budget_max=$a["budget_max"];$time=$a["time"];   
 
     	$response = $a;                    
-	}
 
-        $q2 = "SELECT * FROM `$database`.`transaction` WHERE gig_id=$link AND status=1";
-        $result_set2 = mysql_query($q2);    
-        if (mysql_num_rows($result_set2) == 1) 
-        {
-        	$num_rows = 1;
-        	$response['num_rows'] = $num_rows;
+        if($status==1)
+      	{
+        	$q2 = "SELECT * FROM `$database`.`transaction` WHERE gig_id=$link AND status=1";
+        	$result_set2 = mysql_query($q2);    
+        	if (mysql_num_rows($result_set2) == 1) 
+        	{
+        		$num_rows = 1;
+        		$response['num_rows'] = $num_rows;
 
-            $found = mysql_fetch_array($result_set2);
+            	$found = mysql_fetch_array($result_set2);
             
-            $artist_id=$found["artist_id"];
-            $response['artist_id'] = $artist_id;
+            	$artist_id=$found["artist_id"];
+            	$response['artist_id'] = $artist_id;
 
-            $artist_name=$found["artist_name"];
-            $response['artist_name'] = $artist_name;
+            	$artist_name=$found["artist_name"];
+            	$response['artist_name'] = $artist_name;
 
-            $SQLe = "SELECT mobile FROM `$database`.`members` WHERE link=$artist_id";
-            $resulte = mysql_query($SQLe);
-			$f = mysql_fetch_assoc($resulte);
-			$response['f'] = $f;			
-        }
+            	$SQLe = "SELECT mobile FROM `$database`.`members` WHERE link=$artist_id";
+            	$resulte = mysql_query($SQLe);
+				$f = mysql_fetch_assoc($resulte);
+				$response['f'] = $f;			
+        	}
         
-		$linker=15999*$link;
-		$response['linker'] = $linker;    
-		
-		error_log("Gig Data".$response);
+			$linker=15999*$link;
+			$response['linker'] = $linker;  
+		}  
+	}	
+	
+	error_log("Gig Data: ".$response);
 
 	$this->load->helper('functions');
 	createResponse($response);
