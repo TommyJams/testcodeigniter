c18e9934ea899102bf3580b74acc58648ce898bb
TommyJams <nikhilk218@gmail.com>
2013-09-08 15:39:17 +0530

    edit profile 1.2

--- include/profile.php
+++ include/profile.php
@@ -1,8 +1,28 @@
 <html>
  <head>
+	<link rel='stylesheet' href='css/edit.css'>
 	<!-- Include the JS files -->
+
+	<script type="text/JavaScript">
+	function show(d)
+	{ document.getElementById('frameprofessional').style.display="none";
+		document.getElementById('frameabout').style.display="none";
+		document.getElementById('framecontact').style.display="none";
+		document.getElementById('framesocial').style.display="none";	
+		document.getElementById(d).style.display="block";	
+	}
+	</script>
+
+	<!--<script type="text/javascript">
+		function init() {
+		document.getElementById('loading').style.display = 'none';
+		}
+		window.onload = init();
+	</script>-->
+
  </head>
  <body>
+
     <div id="blanket" style="display:none;                            background-color:#111;
                             opacity: 0.65;
                             position:absolute;
@@ -10,49 +30,49 @@
                             top:0px;
                             left:0px;
                             width:100%;                            height: 100%; "/>
-    <div id="profil" style="display:none;">
-		<a id="loginBoxClose" href="#" onClick="popup('profil')">
-		</a>
-        <center>
-            <h2>Upload your Profile Picture</h2>
-        </center>
-        <form action="update.php" method="post" enctype="multipart/form-data">
-            <table id="uploadTable" style="margin-top: 30px; width: 100%;">
-                <tbody>
-                    <tr>
-                        <td align="center" style="width: 100%;">
-                            <input name="file" id="image" type="file" size="50" />
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="center" style="width: 100%;">
-                            <span class="hint" style="line-height:10px;">
-                            Valid Image File (.jpg, .png, .bmp)
-                            <br>
-                            Max Size: 150KB
-                            </span>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td align="center" style="width: 100%;">
-                            <input name="submit" id="upload" type="submit" value="Upload Picture"/>
-                        </td>
-                    </tr>
-					<tr>
-						<td align="center" style="width: 100%; padding: 20px;">
-                            OR
-						</td>
-					</tr>
-					<tr>
-						<td align="center" style="width: 100%;">
-							<img src="<? echo'https://graph.facebook.com/'.$username.'/picture'; ?>" style="vertical-align:bottom">
-							<input name="submit" id="upload" type="submit" value="Use Facebook Picture"/>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
-        </form>
-    </div>
+        <div id="profil" style="display:none;">
+			<a id="loginBoxClose" href="#" onClick="popup('profil')">
+			</a>
+            <center>
+                <h2>Upload your Profile Picture</h2>
+            </center>
+            <form action="update.php" method="post" enctype="multipart/form-data">
+                <table id="uploadTable" style="margin-top: 30px; width: 100%;">
+                    <tbody>
+                        <tr>
+                            <td align="center" style="width: 100%;">
+                                <input name="file" id="image" type="file" size="50" />
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="center" style="width: 100%;">
+                                <span class="hint" style="line-height:10px;">
+                                Valid Image File (.jpg, .png, .bmp)
+                                <br>
+                                Max Size: 150KB
+                                </span>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td align="center" style="width: 100%;">
+                                <input name="submit" id="upload" type="submit" value="Upload Picture"/>
+                            </td>
+                        </tr>
+						<tr>
+							<td align="center" style="width: 100%; padding: 20px;">
+                                OR
+							</td>
+						</tr>
+						<tr>
+							<td align="center" style="width: 100%;">
+								<img src="<? echo'https://graph.facebook.com/'.$username.'/picture'; ?>" style="vertical-align:bottom">
+								<input name="submit" id="upload" type="submit" value="Use Facebook Picture"/>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
+        </div>
  
 	<script type="text/javascript">
 		$('#loading-indicator').hide();
