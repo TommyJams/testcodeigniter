472a89368edb16770a9296c70e6dd36f823bb486
TommyJams <tommyjams.bizspark@outlook.com>
2013-09-08 16:52:50 +0530

    dib reaction 3.2

--- application/controllers/promoter.php
+++ application/controllers/promoter.php
@@ -234,29 +234,24 @@ public function mygigs(){
         if (mysql_num_rows($result_set2) == 1) 
         {
         	$num_rows = 1;
+        	$response['num_rows'] = $num_rows;
             $found = mysql_fetch_array($result_set2);
         	$artist_id=$found["artist_id"];
         	$artist_name=$found["artist_name"];
+        	$response['artist_name'] = $artist_name;
         
         	$SQLe = "SELECT mobile FROM `$database`.`members` WHERE link=$artist_id";
         	$resulte = mysql_query($SQLe);
-			$f = mysql_fetch_assoc($resulte);			
+			$f = mysql_fetch_assoc($resulte);
+			$response['f'] = $f['mobile'];			
         }
         else
         {
-        	$linker=15999*$link;	
+        	$linker=15999*$link;
+        	$response['linker'] = $linker; 	
         }  		 	
-	
-		$response['status'] = $status; 
-		$response['gig'] = $gig; 
-		$response['formattedDate'] =  $formattedDate;
-		$response['vtime'] = $vtime;
-		$response['city'] = $city;
-		$response['f'] = $f['mobile'];
-		$response['artist_id'] = $artist_id;
-		$response['artist_name'] = $artist_name;
-		$response['num_rows'] = $num_rows;
-		$response['linker'] = $linker; 
+
+		$response = $a; 
 	}
 	
 	$this->load->helper('functions');
