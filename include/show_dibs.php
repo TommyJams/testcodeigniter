34b3a6b216fe7c7ad43069f89e2f32de7144e627
TommyJams <nikhilk218@gmail.com>
2013-09-11 20:01:28 +0530

    dib reaction 1.55

--- include/show_dibs.php
+++ include/show_dibs.php
@@ -77,12 +77,10 @@ function confirmSubmit()
            	$artist_id=$row[1]; 
 			print("<div style='width:50%; margin-top: 10px; height:18px; text-align: center; float:left;'><a href='promoter.php?id=$artist_id' target='_top' class='whiteHoverRef' style='font-size: 16px;'>$artist_name</a></div>"); 
 		?>
-			<form action="" name="dibReaction" id="dibReaction" method="post">
 				<div style="width:45%; float:left; padding-top:10px; padding-right:5px; height:33px;">
-					<input type="submit" value="Accept" name="accept" id="accept" style="width: 48%; background:#B4F62F; float:left;" onClick="showDibReaction('1631327', '1135503', 1)">
-					<input type="submit" value="Reject" name="reject" id="reject" style="width: 48%; background:#FF3C35; float:right;" onClick="showDibReaction('<?print("$linker");?>', '<?print("$artist_id");?>', 0)">
+					<a href='javascript:;' name="accept" id="accept" style="width: 48%; background:#B4F62F; float:left;"  onClick="showDibReaction('<?print("$linker");?>', '<?print("$artist_id");?>', 1);">
+					<a href='javascript:;' name="reject" id="reject" style="width: 48%; background:#FF3C35; float:right;" onClick="showDibReaction('<?print("$linker");?>', '<?print("$artist_id");?>', 0);">
 				</div>           
-			</form>			
 		<?
 		} 
 
