47eb1b5121ed802b22df99edaa571676d5175a1d
TommyJams <nikhilk218@gmail.com>
2013-09-11 17:10:56 +0530

    dib reaction 1.35

--- application/controllers/promoter.php
+++ application/controllers/promoter.php
@@ -705,6 +705,360 @@ public function reaction(){
 	createResponse($response);
 }
 
+/*
+	ob_start();
+	error_log(1);
+	error_log('1');
+
+	$sessionArray = $this->session->all_userdata();
+	$database = 'tommyjam_test';
+
+	if (!isset($sessionArray['session_id()'])) 
+	{
+		session_start();
+	}
+
+	if(!isset($sessionArray['username']))
+	{
+		redirect('http://testcodeigniter.azurewebsites.net/index');
+		exit;
+	}	
+
+	error_log('2');
+	error_log(2);
+
+	$username=$sessionArray['username'];
+	$password=md5($sessionArray['password']);
+
+	//$link=$_POST["gig"]/15999;
+	$link=$_POST['link'];
+	$artistId=$_POST['artist_id'];
+	$accepted=$_POST['accepted'];
+
+	error_log('3');
+	error_log(3); */
+
+/*	if($accepted == 1)
+	{
+		$SQLs = "UPDATE `$database`.`transaction` SET status=1 WHERE gig_id='$link' AND artist_id='$artistId'";
+		$results = mysql_query($SQLs);
+		if(!$results)
+		{
+			$error = 1;
+			$response['error'] = $error;
+			$this->load->helper('functions');
+			createResponse($response);
+		}
+	
+		error_log('4');
+		$SQLs = "SELECT * FROM `$database`.`transaction` WHERE gig_id='$link'";
+		$results = mysql_query($SQLs);
+		while ($a = mysql_fetch_assoc($results))
+		{
+			$status=$a["status"];
+			$gig=$a["gig_name"];
+			$artist_id=$a["artist_id"];
+			$artist_name=$a["artist_name"];
+			$promoter_name=$a["promoter_name"];
+		
+			$q2 = "SELECT * FROM `$database`.`members` WHERE link='$artist_id'";
+			$result_set2 = mysql_query($q2);	
+			if (mysql_num_rows($result_set2) == 1) 
+			{
+				$found = mysql_fetch_array($result_set2);
+				$artist_name=$found["name"];
+				$artist_email=$found["email"];
+			}
+			$to = $artist_email;
+			$gigname=$gig;
+
+			if($status==1)
+			{
+				$SQLs = "SELECT * FROM `$database`.`shop` WHERE link='$link'";
+				$result1 = mysql_query($SQLs);
+				$a = mysql_fetch_array($result1);
+				{
+					$gig=$a["gig"];$date=$a["venue_date"];$vtime=$a["venue_time"];
+					$promoter_name=$a["promoter_name"];$promoter=$a["promoter"];
+				}
+				$q8 = "INSERT INTO `$database`.`rating` (`id`, `event_date`, `event_time`, `status`, `artist_id`, `artist_name`, `promoter_id`, `promoter_name`, `gig_id`, `gig_name`, `promoter_rate`, `promoter_comment`, `promoter_gig_rate`, `promoter_gig_comment`, `promoter_future`, `artist_rate`, `artist_comment`, `artist_dib_comment`, `artist_future`, `time`)
+											VALUES (NULL, '$date', '$vtime', '2', '$artist_id', '$artist_name', '$promoter', '$promoter_name', '$link', '$gig', '', '', '', '', '', '', '', '', '', CURRENT_TIMESTAMP);";
+				$result_set8 = mysql_query($q8);
+				if (!$result_set8)
+				{
+					$error = 1;
+					$response['error'] = $error;
+					$this->load->helper('functions');
+					createResponse($response);
+				}
+				$acceptedArtist=$artist_name;
+				$subject = "Dib Accepted for $gig";
+
+				$mess="<p style='text-align:left;'>
+				Dear $artist_name,<br><br>
+				Congratulations! Your dib has been accepted for the gig: '$gig' on TommyJams.
+				<br>
+				Please login to your profile on TommyJams and view the host details and contact number in the 'Dibs Status' section.
+				<br>
+				We wish you all the very best for the gig.
+				<br><br>
+				Happy Jamming,
+				<br>
+				Team TommyJams
+				<br><br>
+				</p>
+				<center>
+				<table border='1'>
+					<tr>
+						<td>Gig</td>
+						<td>$gig</td>
+					</tr>
+					<tr>
+						<td>Promoter</td>
+						<td>$promoter_name</td>
+					</tr>
+					<tr>
+						<td>Rate Promoter and Gig</td>
+						<td><a href='http://www.tommyjams.com/beta/artist.php?feed=$link'>RATE</a> (enabled only after the gig)</td>
+					</tr>
+				</table>
+				</center>
+				";
+			}
+			elseif($status==2)
+			{
+				continue;
+			}
+			elseif($status==4)
+			{
+				$subject = "Dib Rejected for $gig";
+				$mess="<p style='text-align:left;'>
+				Dear $artist_name,<br><br>
+				Sorry, your dib for the gig: '$gig' has been rejected by the host.
+				<br>
+				However, there are a lot more gigs to be booked on TommyJams and you'll surely find the right ones for you. Please login to your profile on TommyJams and search for the latest gigs in the 'Find Gigs' section.
+				<br>
+				We wish you all the very best in the future.
+				<br><br>
+				Happy Jamming,
+				<br>
+				Team TommyJams
+				<br><br>
+				</p>
+				<center>
+				<table border='1'>
+					<tr>
+						<td>Gig</td>
+						<td>$gig</td>
+					</tr>
+					<tr>
+						<td>Promoter</td>
+						<td>$promoter_name</td>
+					</tr>
+				</table>
+				</center>
+				";
+			}
+		
+			$sender = "alerts@tommyjams.com";
+			
+			$this->load->helper('mail');
+            send_email($to, $sender, $subject, $mess);
+			
+			$to = "alerts@tommyjams.com";
+			send_email($to, $sender, $subject, $mess);
+		}	
+	
+		$SQLs = "UPDATE `$database`.`transaction` SET status=2 WHERE gig_id='$link' AND status=4";
+		$results2 = mysql_query($SQLs);
+
+		$q2 = "SELECT * FROM `$database`.`members` WHERE fb_id='$username'";
+		$result_set2 = mysql_query($q2);
+		if (mysql_num_rows($result_set2) == 1) 
+		{
+			$found = mysql_fetch_array($result_set2);
+			$promoter_name=$found["name"];
+			$promoter_email=$found["email"];
+			$to = $promoter_email;
+			$subject = "Booked Gig: $gigname";
+			$mess="<p style='text-align:left;'>
+				Dear $promoter_name,<br><br>
+				Congratulations! Your gig: '$gigname' is now booked on TommyJams.
+				<br>
+				Please login to your profile on TommyJams and view the artist details and contact number in the 'My Gigs' section.
+				<br>
+				We wish you all the very best for the gig.
+				<br><br>
+				Happy Jamming,
+				<br>
+				Team TommyJams
+				<br><br>
+				</p>
+				<center>
+				<table border='1'>
+					<tr>
+						<td>Gig</td>
+						<td>$gig</td>
+					</tr>
+					<tr>
+						<td>Artist</td>
+						<td>$acceptedArtist</td>
+					</tr>
+					<tr>
+						<td>Rate Artist and Gig</td>
+						<td><a href='http://www.tommyjams.com/beta/promoter.php?feed=$link'>RATE</a> (enabled only after the gig)</td>
+					</tr>
+				</table>
+				</center>
+				";
+			$sender = "alerts@tommyjams.com";
+			
+			$this->load->helper('mail');
+            send_email($to, $sender, $subject, $mess);
+			
+			$to = "alerts@tommyjams.com";
+			send_email($to, $sender, $subject, $mess);
+		}
+		
+		$response['linker'] = $link;
+		$response['error'] = 0;
+		$response['accept'] = 1;
+		$this->load->helper('functions');
+		createResponse($response);
+	} 
+
+	//elseif($accepted == '0')
+	if($accepted == '0')
+	{
+
+		error_log('5');
+		error_log(5);
+		$SQLs = "UPDATE `$database`.`transaction` SET status=2 WHERE gig_id='$link' AND artist_id='$artist_id'";
+		$results = mysql_query($SQLs);
+
+		$SQLs = "SELECT * FROM `$database`.`transaction` WHERE gig_id='$link' AND artist_id='$artist_id'";
+		$results = mysql_query($SQLs);
+
+		if (mysql_num_rows($results) == 1) 
+		{
+			$found = mysql_fetch_array($results);
+			$artist_name=$found["artist_name"];
+		
+			$gig=$found["gig_name"];
+			$promoter_name=$found["promoter_name"];
+		
+			$q2 = "SELECT * FROM `$database`.`members` WHERE link='$artist_id'";
+			$result_set2 = mysql_query($q2);	
+			if (mysql_num_rows($result_set2) == 1) 
+			{
+				$found = mysql_fetch_array($result_set2);
+				$artist_email=$found["email"];
+			}
+		
+			$to = $artist_email;
+			$subject = "Dib Rejected for $gig";
+			$mess="<p style='text-align:left;'>
+			Dear $artist_name,<br><br>
+			Sorry, your dib for the gig: '$gig' has been rejected by the host.
+			<br>
+			However, there are a lot more gigs to be booked on TommyJams and you'll surely find the right ones for you. Please login to your profile on TommyJams and search for the latest gigs in the 'Find Gigs' section.
+			<br>
+			We wish you all the very best in the future.
+			<br><br>
+			Happy Jamming,
+			<br>
+			Team TommyJams
+			<br><br>
+			</p>
+			<center>
+			<table border='1'>
+				<tr>
+					<td>Gig</td>
+					<td>$gig</td>
+				</tr>
+				<tr>
+					<td>Promoter</td>
+					<td>$promoter_name</td>
+				</tr>
+			</table>
+			</center>
+			";
+			$sender = "alerts@tommyjams.com";
+			
+			$this->load->helper('mail');
+        	send_email($to, $sender, $subject, $mess);
+			
+			$to = "alerts@tommyjams.com";
+			send_email($to, $sender, $subject, $mess);
+		}
+
+		$response['linker'] = $link;
+		$response['error'] = 0;
+		$response['accept'] = 0;
+		$this->load->helper('functions');
+		createResponse($response);
+	}
+	*/
+
+
+	public function recommendArtist(){
+
+	$database = 'tommyjam_test';
+	$link = $_POST["link"]; 
+
+	$SQLs = "SELECT * FROM `$database`.`shop` WHERE link='$link'";
+	$results = mysql_query($SQLs);
+	$a = mysql_fetch_array($results);
+	{
+		$gig=$a["gig"];
+		$date=$a["venue_date"];
+		$promoter_name=$a["promoter_name"];
+	}
+
+		$to = "contact@tommyjams.com";
+		$subject = "Artist recommendation wanted for $gig";
+		$mess="<p style='text-align:left;'>
+		Dear Admin,<br><br>
+		You have received a recommendation request for $gig.
+		<br>
+		</p>
+		<center>
+		<table border='1'>
+			<tr>
+				<td>Gig</td>
+				<td>$gig</td>
+			</tr>
+			<tr>
+				<td>Gig ID</td>
+				<td>$link</td>
+			</tr>
+			<tr>
+				<td>Date</td>
+				<td>$date</td>
+			</tr>
+			<tr>
+				<td>Promoter</td>
+				<td>$promoter_name</td>
+			</tr>
+		</table>
+		</center>
+		";
+		$sender = "alerts@tommyjams.com";
+			
+		$this->load->helper('mail');
+        send_email($to, $sender, $subject, $mess);
+			
+		$to = "alerts@tommyjams.com";
+		send_email($to, $sender, $subject, $mess);
+		
+	$alertMessage = "We shall contact you within 24 hours with a recommendation from among the artists who have applied. Thank you for your patience.";
+	
+	$response = $alertMessage;
+
+	$this->load->helper('functions');
+	createResponse($response);
+}
+
 public function showDibs(){
 
 	ob_start();
