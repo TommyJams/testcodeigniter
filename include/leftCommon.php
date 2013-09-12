6eb36fe58e196046eb5cb86a11f5ae9605686f4d
TommyJams <tommyjams.bizspark@outlook.com>
2013-09-12 17:01:36 +0530

    links change 1.4

--- include/leftCommon.php
+++ include/leftCommon.php
@@ -51,7 +51,7 @@
 
 <div id="leftSidebarBox">
     <ul>
-        <li><a href="aboutus" 	target="_blank" title="About Us" 		alt="About Us"><img src="/images/icons/sidebar/about_1.png"/></a></li>
+        <li><a href="<?php echo base_url();?>aboutus" 	target="_blank" title="About Us" 		alt="About Us"><img src="/images/icons/sidebar/about_1.png"/></a></li>
         <li><a href="terms" 	target="_blank" title="Terms of Use" 	alt="Terms of Use"><img src="/images/icons/sidebar/terms_1.png"/></a></li>
         <!--<li><a href="faq.php" title="FAQ" alt="FAQ"><img src="images/icons/sidebar/faq_1.png" style="width:100%; height:100%;"/></a></li>-->
         <li><a href="careers" 	target="_blank" title="Careers" 		alt="Careers"><img src="/images/icons/sidebar/careers_1.png"/></a></li>
